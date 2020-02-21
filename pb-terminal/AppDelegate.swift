e91811a61bdfb4b6df7ff7ce3dbcdcc7df5c90f8
Valentin Dufois <valentin@dufois.fr>
2020-02-21 17:22:32 -0500

    Initial Commit

---
+++ pb-terminal/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  pb-terminal
+//
+//  Created by Valentin Dufois on 2020-02-21.
+//  Copyright © 2020 Prisme. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ pb-terminal/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  pb-terminal
+//
+//  Created by Valentin Dufois on 2020-02-21.
+//  Copyright © 2020 Prisme. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
